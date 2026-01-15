58460cad7329e8e10c46a4e7c6525a909a28b1d4
“Aaryan” <aaryanm126@gmail.com>
2026-01-15 10:05:09 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creatin g necessary directories for the Medical Chatbot project
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.iypnb
+touch requirements.txt
+
+
+echo "Directory structure and files created successfully."
