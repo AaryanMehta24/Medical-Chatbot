41a1e6ee3cab4ef9e1d24578a4c14af722173c5a
“Aaryan” <aaryanm126@gmail.com>
2026-01-15 10:26:24 +0530

    Update requirements and setup, ignore virtual environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Aaryan Mehta",
+    author_email="aaryanm126@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
